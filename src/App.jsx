cf6b0cc643fc7bed2c9aab106dac5884b4a496b2
n3pite00 <n3pite00@students.oamk.fi>
2025-03-26 15:26:14 +0200

    Yhdistetty firebase, dotenv kesken.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react"
+import { db } from "./firebase/Config"
+import { getDocs, collection } from 'firebase/firestore'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [EmployeeList, setEmployeeList] = useState([]);
+
+  const EmployeeCollection = collection(db, "Employee");
+
+  useEffect(() => {
+    const getEmployeeList = async () => {
+      try {
+        const querySnapshot = await getDocs(EmployeeCollection);
+        const employees = querySnapshot.docs.map(doc => doc.data());
+        setEmployeeList(employees);
+      } catch (err) {
+        console.error("Error fetching employee data: ", err);
+      }
+    };
+
+    getEmployeeList();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <div>
+      <h1>Employee List</h1>
+      <ul>
+        {EmployeeList.map((employee, index) => (
+          <li key={index}>{employee.name}</li> // assuming "name" is a field in the Employee document
+        ))}
+      </ul>
+    </div>
     </>
   )
 }
