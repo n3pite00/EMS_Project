62a19686389c2cd3f917d2728d1ba708ae6ee6d2
n3pite00 <n3pite00@students.oamk.fi>
2025-03-31 17:30:28 +0300

    Suojattu firebase tiedot .env ja luotu toimiva routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
-import { useState, useEffect } from "react"
-import { db } from "./firebase/Config"
-import { getDocs, collection } from 'firebase/firestore'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./Pages/Login"
+
 
 function App() {
-  const [EmployeeList, setEmployeeList] = useState([]);
-
-  const EmployeeCollection = collection(db, "Employee");
-
-  useEffect(() => {
-    const getEmployeeList = async () => {
-      try {
-        const querySnapshot = await getDocs(EmployeeCollection);
-        const employees = querySnapshot.docs.map(doc => doc.data());
-        setEmployeeList(employees);
-      } catch (err) {
-        console.error("Error fetching employee data: ", err);
-      }
-    };
-
-    getEmployeeList();
-  }, []);
-
   return (
-    <>
-       <div>
-      <h1>Employee List</h1>
-      <ul>
-        {EmployeeList.map((employee, index) => (
-          <li key={index}>{employee.name}</li> // assuming "name" is a field in the Employee document
-        ))}
-      </ul>
-    </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,12 @@
+
+
+function Login() {
+
+    return (
+        <>
+        <h1>Testi</h1>
+        </>
+    )
+}
+
+export default Login
