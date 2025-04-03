5edf47777cb70f5de09b2c1952e609c2c47708fb
VeetiHyotyla <n3hyve00@students.oamk.fi>
2025-04-03 12:29:57 +0300

    Linkki login sivulta dashboardiin

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,21 @@
-
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+    const navigate = useNavigate(); 
+
+    const handleLogin = () => {
+
+        navigate("/dashboard");
+    };
 
     return (
-        <>
-        <h1>Testi</h1>
-        </>
-    )
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h1>Testi</h1>
+            <button onClick={handleLogin} style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer" }}>
+                Login
+            </button>
+        </div>
+    );
 }
 
-export default Login
+export default Login;
